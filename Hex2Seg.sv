2cf2edbbe7eed7be1981240a32d4521669fcc2f9
Nathan <nathanpark97@outlook.com>
2018-11-06 20:55:23 -0800

    0_

--- Hex2Seg.sv
+++ Hex2Seg.sv
@@ -7,22 +7,22 @@ logic [6:0] ret;
 
 always_comb begin
 case (hexIn) 
- 5'h0: ret = 7'h3F;
- 5'h1: ret = 7'h06;
- 5'h2: ret = 7'h5B;
- 5'h3: ret = 7'h4F;
- 5'h4: ret = 7'h66;
- 5'h5: ret = 7'h6D;
- 5'h6: ret = 7'h7D;
- 5'h7: ret = 7'h07;
- 5'h8: ret = 7'h7F;
- 5'h9: ret = 7'h6F;
- 5'hA: ret = 7'h77;
- 5'hB: ret = 7'h7C;
- 5'hC: ret = 7'h39;
- 5'hD: ret = 7'h5E;
- 5'hE: ret = 7'h79;
- 5'hF: ret = 7'h71;
+ 5'h0_0: ret = 7'h3F;
+ 5'h0_1: ret = 7'h06;
+ 5'h0_2: ret = 7'h5B;
+ 5'h0_3: ret = 7'h4F;
+ 5'h0_4: ret = 7'h66;
+ 5'h0_5: ret = 7'h6D;
+ 5'h0_6: ret = 7'h7D;
+ 5'h0_7: ret = 7'h07;
+ 5'h0_8: ret = 7'h7F;
+ 5'h0_9: ret = 7'h6F;
+ 5'h0_A: ret = 7'h77;
+ 5'h0_B: ret = 7'h7C;
+ 5'h0_C: ret = 7'h39;
+ 5'h0_D: ret = 7'h5E;
+ 5'h0_E: ret = 7'h79;
+ 5'h0_F: ret = 7'h71;
  default: ret = 7'h00;
  endcase
 end
